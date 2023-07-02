2030a9b323cbcb09a860dfe6e73556df2d4265cb
Boopathi Hari <rboopathi@saleminfotech.com>
2023-07-02 10:31:05 +0530

    commit

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,35 @@
+import useFetch from "../../hooks/useFetch";
+import "./featuredProperties.css";
+
+const FeaturedProperties = () => {
+  const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
+
+  return (
+    <div className="fp">
+      {loading ? (
+        "Loading"
+      ) : (
+        <>
+          {data.map((item) => (
+            <div className="fpItem" key={item._id}>
+              <img
+                src="https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                alt=""
+                className="fpImg"
+              />
+              <span className="fpName">{item.name}</span>
+              <span className="fpCity">{item.city}</span>
+              <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+              {item.rating && <div className="fpRating">
+                <button>{item.rating}</button>
+                <span>Excellent</span>
+              </div>}
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default FeaturedProperties;
